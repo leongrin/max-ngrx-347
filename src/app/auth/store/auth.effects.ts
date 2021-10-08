4d8bcec235a71a96c14600048f7384722de9b9ba
Leon Grin <41086608+leongrin@users.noreply.github.com>
2021-10-08 14:34:55 -0300

    new

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -7,6 +7,7 @@ import {of} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
 import {User} from '../user.model';
+import {AuthService} from '../auth.service';
 
 
 export interface AuthResponseData {
@@ -19,50 +20,15 @@ export interface AuthResponseData {
   registered?: boolean;
 }
 
-let tokenExpirationTimer: any;
-
-const autoLogout= (expirationDuration: number) => {
-  tokenExpirationTimer = setTimeout(() => {
-    return of(new AuthActions.Logout());
-  }, expirationDuration);
-}
-
-const handleError = (errorRes) => {
-  let errorMessage = 'An unknown error occurred!';
-  if (!errorRes.error || !errorRes.error.error) {
-    return of(new AuthActions.AuthenticateFailed(errorMessage));
-  }
-  switch (errorRes.error.error.message) {
-    case 'EMAIL_EXISTS':
-      errorMessage = 'This email exists already';
-      break;
-    case 'EMAIL_NOT_FOUND':
-      errorMessage = 'This email does not exist.';
-      break;
-    case 'INVALID_PASSWORD':
-      errorMessage = 'This password is not correct.';
-      break;
-  }
-  return of(new AuthActions.AuthenticateFailed(errorMessage));
-}
-
-const handleAuthentication = (resData) => {
-  const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
-  const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
-  localStorage.setItem('userData', JSON.stringify(user));
-
-  autoLogout(resData.expiresIn);
-
-  return new AuthActions.AuthenticateSuccess({
-    email: resData.email,
-    userId: resData.localId,
-    expirationDate: expirationDate,
-    token: resData.idToken
-  })
-}
 
 @Injectable()
 export class AuthEffects {
+
+  constructor(private actions$: Actions,
+              private http: HttpClient,
+              private router: Router,
+              private authServ: AuthService) {
+  }
 
   authLogin$ = createEffect(() => {
     return this.actions$.pipe(
@@ -77,11 +43,11 @@ export class AuthEffects {
               returnSecureToken: true
             }
           ).pipe(
-            map(resData => {
-              return handleAuthentication(resData)
+            map((resData) => {
+              return this.handleAuthentication(resData)
             }),
             catchError(errorRes => {
-              return handleError(errorRes);
+              return this.handleError(errorRes);
             }))
       })
     );
@@ -111,11 +77,11 @@ export class AuthEffects {
                 returnSecureToken: true
               }
             ).pipe(
-              map(resData => {
-                return handleAuthentication(resData)
+              map((resData) => {
+                return this.handleAuthentication(resData)
               }),
               catchError(errorRes => {
-                return handleError(errorRes);
+                return this.handleError(errorRes);
               })
             )
         })
@@ -129,10 +95,7 @@ export class AuthEffects {
       ofType(AuthActions.LOGOUT),
       tap(() => {
         localStorage.removeItem('userData');
-        if (tokenExpirationTimer) {
-          clearTimeout(tokenExpirationTimer);
-        }
-        tokenExpirationTimer = null;
+        this.authServ.clearLogoutTimer();
       })
     );
   }, {dispatch: false})
@@ -162,6 +125,10 @@ export class AuthEffects {
 
         if (loadedUser.token) {
 
+          const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+
+          this.authServ.setLogoutTimer(expirationDuration);
+
           return new AuthActions.AuthenticateSuccess({
             email: userData.email,
             userId: userData.id,
@@ -172,13 +139,43 @@ export class AuthEffects {
           return { type: 'DUMMY'};
         }
       })
-
     )
-  )
+  );
+
+  handleAuthentication(resData) {
+    const expirationDate = new Date(new Date().getTime() + +resData.expiresIn * 1000);
+    const user = new User(resData.email, resData.localId, resData.idToken, expirationDate);
+    localStorage.setItem('userData', JSON.stringify(user));
+
+    this.authServ.setLogoutTimer(+resData.expiresIn * 1000);
+
+    return new AuthActions.AuthenticateSuccess({
+      email: resData.email,
+      userId: resData.localId,
+      expirationDate: expirationDate,
+      token: resData.idToken
+    })
+  };
 
 
-  constructor(private actions$: Actions,
-              private http: HttpClient,
-              private router: Router) {
+  handleError(errorRes) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorRes.error || !errorRes.error.error) {
+      return of(new AuthActions.AuthenticateFailed(errorMessage));
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct.';
+        break;
+    }
+    return of(new AuthActions.AuthenticateFailed(errorMessage));
   }
+
+
 }
